5de2b3fe901de21a295a49049bd4bae4029ec1f2
Prabanjan Jeyasankar <prabanjanjeyasankar@gmail.com>
2023-04-17 13:50:13 +0530

    Add. Initial Commit done to Save File Structure

---
+++ src/Component/Routes/MemoryFlipComponent/MemoryFlipComponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class MemoryFlipComponent extends Component {
+    
+  render() {
+    return (
+    <h1 className='text-3xl font-bold underline'>MemoryFlipComponent</h1>
+     )
+  }
+}
+
+export default MemoryFlipComponent
